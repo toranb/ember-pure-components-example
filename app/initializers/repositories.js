41ba69356fc40b5152716e0a320358a706d01f1b
toran billups <toranb@gmail.com>
2016-03-08 20:45:55 -0600

    ember-cli upgrade to 2.4.2 and ember upgrade to 2.4.1

--- app/initializers/repositories.js
+++ app/initializers/repositories.js
@@ -1,12 +1,13 @@
 import registerWithContainer from "ember-cli-auto-register/register";
 
-export function initialize(container, application) {
+export function initialize() {
+    var application = arguments[1] || arguments[0];
     registerWithContainer("repositories", application);
     application.inject("repositories", "store", "store:main");
 }
 
 export default {
     name: "repositories",
-    after: "store",
+    after: "simple-store",
     initialize: initialize
 };
