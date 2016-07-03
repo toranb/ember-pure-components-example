517a00238dac35e269488189016f92df48c35304
toran billups <toranb@gmail.com>
2016-07-03 17:22:08 -0500

    updated to simple-store v4.3.0

--- app/repositories/customer.js
+++ app/repositories/customer.js
@@ -2,8 +2,9 @@ import Ember from "ember";
 import PromiseMixin from "ember-promise/mixins/promise";
 
 export default Ember.Object.extend({
+    simpleStore: Ember.inject.service(),
     find: function() {
-        var store = this.get("store");
+        var store = this.get('simpleStore');
         var all = store.find("customer");
         PromiseMixin.xhr("/api/customers").then(function(response) {
             response.forEach(function(data) {
